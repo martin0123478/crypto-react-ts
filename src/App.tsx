af435252c77b2a863695e73082a467a2a76268cd
martin0123478 <martin_agui97@outlook.es>
2024-09-09 16:43:48 -0600

    feat:basic design and styles

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container">
+        <h1 className='app-title'>Cotizador de <span>Criptomonedas</span></h1>
+        <div className="content">
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
